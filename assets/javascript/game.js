3b9d38f0aae89704dc3833131808480710504e19
Arielle <ariellcollinsb@gmail.com>
2019-09-10 00:00:41 -0600

    basic game is working but bonus actions need to be added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,15 +9,18 @@ var wordList = ["host", "logo", "code", "ford", "claw",
 
 var answer = "";
 var matches = "";
-
+var wins = 0;
+var losses = 0;
 
 function start() {
     answer = getRandomWord();
     matches = "";
+    lives = 10;
     for (i = 0; i < answer.length; i++) {
         matches = matches + "_";
     }
     display();
+    document.querySelector("#start-button").innerHTML = "Restart";
     console.log(answer);
 }
 
@@ -29,26 +32,57 @@ function getRandomWord() {
 
 function display() {
     document.querySelector("#matches").innerHTML = matches.split("").join(" ");
+    document.querySelector(".lives").innerHTML = lives;
+    document.querySelector(".wins").innerHTML = wins;
+    document.querySelector(".losses").innerHTML = losses;
+   // document.querySelector(".you-win").innerHTML = "YOU WIN!!";
+
+
+}
+
+document.onkeyup = function (event) {
+    userGuess(event.key);
 
 }
 
 function userGuess(guess) {
     var newMatches = "";
+    var found = false;
     for (j = 0; j < answer.length; j++) {
 
         if (guess === answer[j]) {
 
             newMatches = newMatches + answer[j];
 
+            found = true;
+
         } else {
             newMatches = newMatches + matches[j];
         }
-    
     }
     matches = newMatches;
 
-display();
+
+    if (found === false) {
+        lives -= 1;
+        if (lives === 0) {
+            display();
+           // document.querySelector(".you-win").innerHTML = "YOU LOOSE";
+            losses += 1;
+            alert("Boo Hoo!")
+        }
+    } else if (answer === newMatches) {
+        console.log(newMatches);
+        display();
+        //document.querySelector(".you-win").innerHTML = "YOU WIN!!";
+        wins += 1;
+        alert("You Win")
+        
+    }
+
+    display();
 }
+
 
 
 document.querySelector("#start-button").addEventListener("click", function () {
